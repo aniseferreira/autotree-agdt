dc61ba349e4eb953cceb056b3a430f53fcb35c59
Anise D'Orange Ferreira <aniseferreira@users.noreply.github.com>
2026-08-19 17:17:44 -0300

    Update app.py

--- app.py
+++ app.py
@@ -969,12 +969,15 @@ def aplicar_estrutura_aci_e_disjuncao(words):
             oion["new_head"] = conj_eta_app["id"]
             oion["lock"] = True
 
-        for id_w in ["17", "19"]:
+for id_w in ["17", "19"]:
             w = get_word(words, id_w)
             if w:
                 w["new_rel"] = "APOS_CO"
                 w["new_head"] = conj_eta_app["id"]
-                w["lock"] = Truedef get_word(words, target_id):
+                w["lock"] = True
+
+
+def get_word(words, target_id):
     """Retorna o dicionário da palavra com o id especificado (suporta int ou str)."""
     if target_id is None:
         return None
